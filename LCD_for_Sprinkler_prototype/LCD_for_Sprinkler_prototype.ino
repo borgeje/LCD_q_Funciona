79c89fbcd143e694fc0cfcd5a74eaedb932baa80
borgeje <borgeje@JB-Macbook-Pro.local>
2017-05-07 00:19:52 -0400

    Updates

--- LCD_for_Sprinkler_prototype/LCD_for_Sprinkler_prototype.ino
+++ LCD_for_Sprinkler_prototype/LCD_for_Sprinkler_prototype.ino
@@ -46,65 +46,59 @@ byte VertBar[8] = {
   B00100,
   B00100
 };
+
 byte FifteenMin[8] = {0x0, 0xe, 0x15, 0x17, 0x11, 0xe, 0x0}; // fetching time indicator
 byte raindrop[8] = {0x4, 0x4, 0xA, 0xA, 0x11, 0xE, 0x0,}; // fetching Valve Data indicator
 
 // General Variables
 int Clock_Animation_time=200;
+unsigned long startMillis;
+bool DOWNbuttonPushed = false;
+
+//Hardware
+const int DOWN_Pin = 3;
 
 //others
 #define VALVE_RESET_TIME 7500UL 
 
+
+/********************/
 void setup()
 {
  //    Basic Initiatization procedures //
-  lcd.init();                      // initialize the lcd 
-  lcd.init();
-  lcd.createChar(0, Bar1);
-  lcd.createChar(1, Bar2);
-  lcd.createChar(2, ParBar);
-  lcd.createChar(3, VertBar);
-  lcd.createChar(4, FifteenMin);
-  lcd.createChar(5, raindrop);
-  lcd.backlight();
+  lcd.init();                       // initialize the LCD
+  lcd.init();                       // initialize the LCD
+  lcd.createChar(0, Bar1);          // Create custom character in LCD
+  lcd.createChar(1, Bar2);          // Create custom character in LCD
+  lcd.createChar(2, ParBar);        // Create custom character in LCD
+  lcd.createChar(3, VertBar);       // Create custom character in LCD
+  lcd.createChar(4, FifteenMin);    // Create custom character in LCD
+  lcd.createChar(5, raindrop);      // Create custom character in LCD
+  lcd.backlight();                  // Turn on Back light (to tur off is lcd.noBacklight()
+  attachInterrupt(digitalPinToInterrupt(DOWN_Pin), DownButtonPress, RISING);
+  
   //////// MAIN INTRO SCREEN, print and stay 2s /////////
   lcd.setCursor(0,0);
   lcd.print("Sprinkler     v1.1");
   lcd.setCursor(2,1);
   lcd.print("My Sensors System");
-   lcd.setCursor(0,2);
+  lcd.setCursor(0,2);
   lcd.print("              ");
-   lcd.setCursor(2,3);
+  lcd.setCursor(2,3);
   lcd.print("Joao Borges");
   delay(2000);                // REPLACE WITH WAIT WHEN INTEGRATING WITH MYSENSORS
-  lcd.setCursor(0,2);
-  lcd.print("  Initializing.......");
-  lcd.setCursor(1,1);
-  lcd.print("Zone1 Time: 03:59 x");
-  lcd.setCursor(8,1);
-  lcd.write(4);
-  lcd.setCursor(0,2);
-  lcd.print("");
-  lcd.setCursor(10,3);
-  lcd.write(5);
-  delay(200);
+  lcd.init();
+  MainMenu();
 }
 
 
 void loop()
 {
-  lcd.setCursor(19,1);
-  lcd.write(0);
-  delay(Clock_Animation_time);
-  lcd.setCursor(19,1);
-  lcd.write(3);
-  delay(Clock_Animation_time);
-  lcd.setCursor(19,1);
-  lcd.write(1);
-  delay(Clock_Animation_time);
-  lcd.setCursor(19,1);
-  lcd.write(2);
-  delay(Clock_Animation_time);
+  
+  if (DOWNbuttonPushed)
+  {
+    
+  }
 }
 
 
